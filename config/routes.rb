5143a9df8b560ad780445037a08b93c6cd7557b7
Selenophilia <pauloargenal@gmail.com>
2020-09-04 20:34:35 +0800

    Modified root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #root route
-  root   'login#new'
+  root   'login#welcome'
 
   # user route start
   get '/users', to: 'users#index'
