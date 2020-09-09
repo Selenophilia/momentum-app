f4890510aaf80c25c7c782f1bbe5088893026ede
Selenophilia <pauloargenal@gmail.com>
2020-09-09 21:28:31 +0800

    Modified quotation_tag test model

--- test/models/quotation_tag_test.rb
+++ test/models/quotation_tag_test.rb
@@ -1,18 +1,24 @@
 require 'test_helper'
 
 class QuotationTagTest < ActiveSupport::TestCase
-  # test "should  save if all of the fields are existing" do
-  #   tag = QuotationTag.new(tag_id: 1 , quotation_id: 1)
-  #   assert tag.save, "Tag save" 
-  # end   
+  setup do 
+    @tag    = Tag.create(name: "sample_tag_name")
+    @author = Author.create(author_name: "sample_author_name") 
+    @quote  = Quote.create(description: "sample_quote_name", author_id: @author.id)
+  end 
 
-  # test "should not save if tag_id  is not existing" do
-  #   tag = QuotationTag.new(tag_id: "", quotation_id: 1)
-  #   assert_not tag.save, "Tag save" 
-  # end 
+  test "should not save if tag_id  is not existing" do
+    tag = QuotationTag.new(tag_id: "", quote_id: 1)
+    assert_not tag.save, "Tag not save" 
+  end 
 
-  # test "should not save if quotation_id  is not existing" do
-  #   tag = QuotationTag.new(tag_id: 1 , quotation_id: "")
-  #   assert_not tag.save, "Tag save" 
-  # end 
+  test "should not save if quotation_id  is not existing" do
+    tag = QuotationTag.new(tag_id: 1 , quote_id: "")
+    assert_not tag.save, "Tag not save" 
+  end 
+
+  test "should  save if all fields are present" do
+    @quotation_tag = QuotationTag.new(tag_id: @tag.id , quote_id: @quote.id)
+    assert @quotation_tag.save, "Tag save" 
+  end 
 end
