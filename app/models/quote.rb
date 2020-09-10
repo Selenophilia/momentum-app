fd14b1270d8fb8b630cff5cbd6e13503334494f9
Selenophilia <pauloargenal@gmail.com>
2020-09-10 21:44:03 +0800

    Added nested attribute association in quote,tag,quotation_tag

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,8 +1,15 @@
 class Quote < ApplicationRecord
   belongs_to :author
   has_many :quotation_tags
-  has_many :tags, 
-            through: :quotation_tag  
+  has_many :tags,:through => :quotation_tags   
+  accepts_nested_attributes_for :tags, allow_destroy: true
+  accepts_nested_attributes_for :quotation_tags
 
-  
+  validates_associated :quotation_tags          
+  validates_associated :tags
+
+  # validates_presence_of :quotation_tags          
+  # validates_presence_of :tags
+
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,18 @@
 class Tag < ApplicationRecord
 
-    has_many  :quotation_tag
-    has_many  :quote, 
-              through: :quotation_tag   
+    has_many  :quotation_tags
+    has_many  :quotes,:through =>  :quotation_tags   
+    accepts_nested_attributes_for :quotes, allow_destroy: true
+    accepts_nested_attributes_for :quotation_tags
+
     validates :name, presence: true, 
               length: {minimum: 4}
+    
+    validates_associated :quotes          
+    validates_associated :quotation_tags          
+
+    # validates_presence_of :quotes          
+    # validates_presence_of :quotation_tags          
+
 
 end
