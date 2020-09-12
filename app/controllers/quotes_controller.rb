0b7e1578ee28d9c34fd2abf4e376cef791c4e422
Selenophilia <pauloargenal@gmail.com>
2020-09-12 09:43:00 +0800

    Modified quote controllers create and update params

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,20 +2,23 @@ class QuotesController < ApplicationController
     def index
         @author = Author.where(id: params[:author_id])
         @quotes = Quote.where(author_id: params[:author_id])
-        @get_tag = Tag.joins(:quotation_tags).where(quotation_tags: { quote_id: @quotes.ids }).first
+        @get_tag  = Tag.joins(quotation_tags: :quote ).limit(1)
+        
     end 
    
     def new
         @quote = Quote.new
+        @tag = Tag.all
+
     end
 
     def create
-        @tag_id = params[:tag_ids].to_i
-        @quote = Quote.create(params.permit(:description, :author_id, tag_ids: [])) 
+        @quote = Quote.new(quote_params)
+        byebug
 
         if  @quote.save 
-            @quote_tags = @quote.quotation_tags.create(tag_id: @tag_id)
-            @quote_tags.save
+            # @quote_tags = @quote.quotation_tags.create(tag_id: @tag_id)                  
+            # @quote_tags.save
             redirect_to authors_quotes_path
         else
             flash[:errors] = @quote.errors.full_messages 
@@ -31,8 +34,8 @@ class QuotesController < ApplicationController
         if is_admin? 
             @quotes = Quote.find(params[:id])
             @author = Author.find(params[:author_id])               
-            @get_tag = Tag.joins(:quotation_tags).where(quotation_tags: { quote_id: params[:id] }).first            
-
+            #@get_tag = @quotes.tags.first 
+            #Tag.joins(:quotation_tags).where(quotation_tags: { quote_id: params[:id] }).first            
         else    
             flash[:errors] = "You are not Authorized to that!"
             redirect_to authors_quotes_path
@@ -42,10 +45,12 @@ class QuotesController < ApplicationController
     def update
         @quotes = Quote.find(params[:id])
         @author = Author.find(params[:author_id])  
-        #byebug
         if @quotes
-            @quotes.update(params.permit(:description, :author_id))
+            #byebug
+            @quotes.update(quote_params)
+            
             @author.update(params.permit(:author_name))
+            
             #redirect_to show_author_quotes_path
             redirect_to authors_quotes_path
         else
@@ -61,6 +66,12 @@ class QuotesController < ApplicationController
             @quotes.destroy                
             redirect_to authors_quotes_url
         end 
+    end
+
+    private
+
+    def quote_params
+        params.permit(:description, :author_id, tag_ids: []) 
     end
 
 end 
